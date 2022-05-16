2fc5526938e13e7e5a439aadd53a3714fe1a945b
KhalilAMARDJIA <47823346+KhalilAMARDJIA@users.noreply.github.com>
2022-05-16 11:48:02 +0200

    Update pdfmerge.py

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,20 +1,24 @@
-import os
-os.chdir("I:\\Python_merge_pdf")
-import glob
-from PyPDF2 import PdfFileMerger
-from PyPDF2.generic import Bookmark
-def merger(output_path, input_paths):
-    pdf_merger = PdfFileMerger()
-    file_handles = []
-    
-    for path in input_paths:
-        bookmark = os.path.basename(path)
-        pdf_merger.append(path,import_bookmarks=False, bookmark = bookmark)
+def main():
+    import os
+    os.chdir("H:\\AFF-CLINIQUE\\Produits\\NEOMIS Percutaneous\\2022_CER V00 MDR Burrs\\ANNEXES\\ANNEX D\\all_articles")
+    import glob
+    from PyPDF2 import PdfFileMerger
+    from PyPDF2.generic import Bookmark
+    def merger(output_path, input_paths):
+        pdf_merger = PdfFileMerger()
+        file_handles = []
         
-    with open(output_path, 'wb') as fileobj:
-        pdf_merger.write(fileobj)
-        
-if __name__ == '__main__':
-    paths = glob.glob('*.pdf')
-    paths.sort()
-    merger('pdf_merger2.pdf', paths)
+        for path in input_paths:
+            bookmark = os.path.basename(path)
+            pdf_merger.append(fileobj= path,bookmark=None, pages=None, import_bookmarks=False)
+            
+        with open(output_path, 'wb') as fileobj:
+            pdf_merger.write(fileobj)
+            
+    if __name__ == '__main__':
+        paths = glob.glob('*.pdf')
+        paths.sort()
+        merger(output_path= 'pdf_merger.pdf', input_paths= paths)
+
+if __name__ == "__main__":
+    main()
